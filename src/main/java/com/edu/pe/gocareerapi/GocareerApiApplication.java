2cb2874abd5b6236bd5011f86201b1b4a0fe7a1d
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-10-13 20:11:23 -0500

    init project

---
+++ src/main/java/com/edu/pe/gocareerapi/GocareerApiApplication.java
@@ -0,0 +1,13 @@
+package com.edu.pe.gocareerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GocareerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GocareerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/pe/gocareerapi/GocareerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.pe.gocareerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GocareerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
