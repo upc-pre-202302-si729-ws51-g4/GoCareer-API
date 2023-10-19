b1c88deaf0e92203b893c74c3438d11f31b5cedd
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-10-19 02:36:58 -0500

    (feat) user name value object added

--- src/main/java/com/edu/pe/gocareerapi/User/domain/model/valueObjects/UserName.java
+++ src/main/java/com/edu/pe/gocareerapi/User/domain/model/valueObjects/UserName.java
@@ -1,4 +1,21 @@
 package com.edu.pe.gocareerapi.User.domain.model.valueObjects;
 
-public record UserName() {
+public record UserName(String firstName, String lastName) {
+    public UserName(){
+        this(null,null);
+    }
+
+    public UserName{
+        if(firstName == null || firstName.isBlank()){
+            throw new IllegalArgumentException("First name cannot be blank");
+        }
+
+        if(lastName == null || lastName.isBlank()) {
+            throw new IllegalArgumentException("Last name cannot be blank");
+        }
+    }
+
+    public String getUserName(){
+        return firstName + " " + lastName;
+    }
 }
--- src/main/java/com/edu/pe/gocareerapi/User/infrastructure/repositories/UserRepository.java
+++ src/main/java/com/edu/pe/gocareerapi/User/infrastructure/repositories/UserRepository.java
@@ -4,9 +4,7 @@ import com.edu.pe.gocareerapi.User.domain.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-
-
-}
+public interface UserRepository extends JpaRepository<User, Integer> {}
