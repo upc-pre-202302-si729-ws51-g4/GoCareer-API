74b578e6bfd9377c27b8c446b0175c026df26b28
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-10-24 04:11:49 -0500

    (fix) init Application modified

--- src/main/java/com/edu/pe/gocareerapi/GocareerApiApplication.java
+++ src/main/java/com/edu/pe/gocareerapi/GocareerApiApplication.java
@@ -2,7 +2,10 @@ package com.edu.pe.gocareerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class GocareerApiApplication {
 
