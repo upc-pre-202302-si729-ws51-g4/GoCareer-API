6ac4a701d398e54397ddd3086559c7952533a8f2
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-10-24 04:12:23 -0500

    (fix) Auditable model modified

--- src/main/java/com/edu/pe/gocareerapi/shared/model/AuditableModel.java
+++ src/main/java/com/edu/pe/gocareerapi/shared/model/AuditableModel.java
@@ -1,13 +1,16 @@
 package com.edu.pe.gocareerapi.shared.model;
 
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Data
 public class AuditableModel {
