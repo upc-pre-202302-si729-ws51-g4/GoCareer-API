20808e581c740730471b782a42967a25f969116e
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-10-24 04:16:49 -0500

    (feat) Reunion entity added

--- src/main/java/com/edu/pe/gocareerapi/Meetings/domain/model/entities/Reunion.java
+++ src/main/java/com/edu/pe/gocareerapi/Meetings/domain/model/entities/Reunion.java
@@ -1,19 +1,21 @@
 package com.edu.pe.gocareerapi.Meetings.domain.model.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.edu.pe.gocareerapi.Meetings.domain.model.aggregates.Student;
+import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.util.Date;
+
 @Entity
-public class Meeting {
+public class Reunion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     private Long id;
 
-    private 
+    private String description;
+
+    private Date date;
 
 }
