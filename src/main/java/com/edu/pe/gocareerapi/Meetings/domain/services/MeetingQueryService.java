26820f3d6ce86337a26169be64ecb4c17f89dab4
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-11-05 17:11:58 -0500

    (fix) meeting query service modified

--- src/main/java/com/edu/pe/gocareerapi/Meetings/domain/services/MeetingQueryService.java
+++ src/main/java/com/edu/pe/gocareerapi/Meetings/domain/services/MeetingQueryService.java
@@ -1,4 +1,15 @@
 package com.edu.pe.gocareerapi.Meetings.domain.services;
 
+import com.edu.pe.gocareerapi.Meetings.domain.model.aggregates.Meeting;
+import com.edu.pe.gocareerapi.Meetings.domain.model.queries.GetAllMeetingsQuery;
+import com.edu.pe.gocareerapi.Meetings.domain.model.queries.GetMeetingByIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MeetingQueryService {
+
+    Optional<Meeting> handle(GetMeetingByIdQuery query);
+
+    List<Meeting> handle(GetAllMeetingsQuery query);
 }
