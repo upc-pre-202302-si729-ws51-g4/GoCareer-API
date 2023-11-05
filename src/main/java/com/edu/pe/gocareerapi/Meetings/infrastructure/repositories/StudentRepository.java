00758f833f8a35284149c971dd1caa97ceb208ed
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-11-05 17:30:02 -0500

    (feat) student repository  added

--- src/main/java/com/edu/pe/gocareerapi/Meetings/infrastructure/repositories/StudentRepository.java
+++ src/main/java/com/edu/pe/gocareerapi/Meetings/infrastructure/repositories/StudentRepository.java
@@ -1,6 +1,8 @@
 package com.edu.pe.gocareerapi.Meetings.infrastructure.repositories;
 
 import com.edu.pe.gocareerapi.Meetings.domain.model.aggregates.Student;
+import com.edu.pe.gocareerapi.Meetings.domain.model.valueObjects.ProfileId;
+import com.edu.pe.gocareerapi.Meetings.domain.model.valueObjects.StudentRecordId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,7 @@ import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    Optional<Student> findByProfileId(String studentRecordId);
+    Optional<Student> findStudentByRecordId(StudentRecordId studentRecordId);
+
+    Optional<Student> findStudentByProfileId(ProfileId profileId);
 }
