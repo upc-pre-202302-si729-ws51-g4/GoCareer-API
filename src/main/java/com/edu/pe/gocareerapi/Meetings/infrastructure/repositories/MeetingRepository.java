8cd0ac0e86a50acd400c586401a1a96fdbca59bd
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-11-05 17:36:39 -0500

    (feat) meeting repository added

--- src/main/java/com/edu/pe/gocareerapi/Meetings/infrastructure/repositories/MeetingRepository.java
+++ src/main/java/com/edu/pe/gocareerapi/Meetings/infrastructure/repositories/MeetingRepository.java
@@ -1,8 +1,14 @@
 package com.edu.pe.gocareerapi.Meetings.infrastructure.repositories;
 
-import com.edu.pe.gocareerapi.Meetings.domain.model.entities.MeetingItem;
+
+import com.edu.pe.gocareerapi.Meetings.domain.model.aggregates.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface MeetingRepository extends JpaRepository<MeetingItem,Long> {
+import java.util.List;
+import java.util.Optional;
 
+@Repository
+public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+    Optional<Meeting> findById(Long id);
 }
