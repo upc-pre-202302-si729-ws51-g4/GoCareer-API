b6d0d31298471fb7ed768b137c7244fb39b44c8d
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-11-05 17:13:59 -0500

    (feat) student by record id added

---
+++ src/main/java/com/edu/pe/gocareerapi/Meetings/domain/model/queries/GetStudentByStudentRecordId.java
@@ -0,0 +1,6 @@
+package com.edu.pe.gocareerapi.Meetings.domain.model.queries;
+
+import com.edu.pe.gocareerapi.Meetings.domain.model.valueObjects.StudentRecordId;
+
+public record GetStudentByStudentRecordId(StudentRecordId studentRecordId) {
+}
