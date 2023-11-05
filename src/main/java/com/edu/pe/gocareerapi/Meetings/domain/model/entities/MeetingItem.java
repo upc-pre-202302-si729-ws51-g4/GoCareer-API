843e51f6454b2e83b21f6f55dcc53e319adb14b8
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-11-05 12:41:30 -0500

    meeting item added

--- src/main/java/com/edu/pe/gocareerapi/Meetings/domain/model/entities/MeetingItem.java
+++ src/main/java/com/edu/pe/gocareerapi/Meetings/domain/model/entities/MeetingItem.java
@@ -1,6 +1,7 @@
 package com.edu.pe.gocareerapi.Meetings.domain.model.entities;
 
 
+import com.edu.pe.gocareerapi.Meetings.domain.model.aggregates.Meeting;
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -11,11 +12,41 @@ public class MeetingItem {
     @Getter
     private Long id;
 
-    /*
+
     @ManyToOne(optional = false)
     @JoinColumn(name = "meeting_id")
-    private
+    private Meeting meeting;
 
-     */
+    @Getter
+    @ManyToOne
+    @JoinColumn(name = "next_meeting_id")
+    private MeetingItem nextItem;
+
+    @Getter
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "reunion_id")
+    private Reunion reunion;
+
+    public MeetingItem(Meeting meeting, Reunion reunion, MeetingItem nextItem){
+        this.meeting = meeting;
+        this.reunion = reunion;
+        this.nextItem = nextItem;
+    }
+
+    public MeetingItem(){
+        this.meeting = null;
+        this.nextItem = null;
+    }
+
+
+    public void updateNextItem(MeetingItem nextItem){
+        this.nextItem = nextItem;
+    }
+
+
+
+
+
+
 
 }
