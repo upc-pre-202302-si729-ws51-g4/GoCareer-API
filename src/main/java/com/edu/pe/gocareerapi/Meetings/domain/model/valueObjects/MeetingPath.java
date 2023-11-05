127beb2c1fa140fc39f472aaf30e3f05b7068b07
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-11-05 17:50:26 -0500

    (feat) meetingpath value object added

--- src/main/java/com/edu/pe/gocareerapi/Meetings/domain/model/valueObjects/MeetingPath.java
+++ src/main/java/com/edu/pe/gocareerapi/Meetings/domain/model/valueObjects/MeetingPath.java
@@ -1,7 +1,9 @@
 package com.edu.pe.gocareerapi.Meetings.domain.model.valueObjects;
 
 
+import com.edu.pe.gocareerapi.Meetings.domain.model.aggregates.Meeting;
 import com.edu.pe.gocareerapi.Meetings.domain.model.entities.MeetingItem;
+import com.edu.pe.gocareerapi.Meetings.domain.model.entities.Reunion;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.OneToMany;
 
@@ -17,6 +19,40 @@ public class MeetingPath {
 
     public MeetingPath(){
         this.meetingItems = new ArrayList<>();
+    }
+
+
+    public void addItem(Meeting meeting, Reunion reunion){
+        MeetingItem lastMeeting = getLastItem();
+
+        MeetingItem meetingItem = new MeetingItem(meeting,reunion,null);
+        meetingItems.add(meetingItem);
+        if(lastMeeting != null) lastMeeting.updateNextItem(meetingItem);
+    }
+
+    public void addItem(Meeting meeting, Reunion reunion, Long nextItem){
+        MeetingItem meetingItem = getMeetingItemWithReunionId(nextItem);
+        addItem(meeting,reunion, meetingItem.getId());
+    }
+
+
+
+    private MeetingItem getReunionWithId(Long meetingId){
+        return meetingItems.stream()
+                .filter(meetingItem -> meetingItem.getId().equals(meetingId))
+                .findFirst().orElse(null);
+    }
+
+
+    private MeetingItem getMeetingItemWithReunionId(Long reunionId){
+        return meetingItems.stream()
+                .filter(meetingItem -> meetingItem.getReunion().getId().equals(reunionId))
+                .findFirst().orElse(null);
+    }
+
+
+    private MeetingItem getLastItem() {
+        return meetingItems.stream().filter(item -> item.getNextItem() == null).findFirst().orElse(null);
     }
 
 
